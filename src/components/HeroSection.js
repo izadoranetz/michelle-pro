ec4db50a73140a16dd15ba4cea4a9f92d992eb5b
izadoranetz <izadora.ns@gmail.com>
2022-11-08 16:40:48 -0300

    ajustes no alinhamento hero e scss modules

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import './herosection.scss';
+import * as styles from './herosection.module.scss';
 import fotoCrianca from '../assets/images/foto-home.png';
-import arcoIris from '../assets/images/arco-iris.svg';
-import estrelinhas from '../assets/images/estrelinhas.svg';
 
 function HeroSection() {
   return (
-    <div id="home" className="hero-section">
+    <div id="home" className={styles.heroSection}>
       <div className="container">
         <div className="row align-items-center position-relative">
           <div className="col-lg-6 col-sm-6">
-            <div className="hero-content">
-              <h1 className="titulo-principal col-lg-6 col-md-6">
+            <div className="container">
+              <h1 className={`col-lg-6 col-md-6 ${styles.tituloPrincipal}`}>
                 <span>É sobre </span>
                 <span>cuidado</span>
               </h1>
-              <p className="subtitulo col-lg-8">
+              <p className={`col-lg-8 ${styles.subtitulo}`}>
                 Conheça a clínica referência em intervenção precoce para
                 crianças com autismo
               </p>
             </div>
           </div>
           <div className="col-lg-6 col-sm-6">
-            <div className="hero-imagens">
-                <div className="row">
-                    <img className="img-crianca" src={fotoCrianca} alt="Uma criança usando máscara sobre os olhos" />
+            <div className={styles.heroImagens}>
+                <div className="row d-flex justify-content-center">
+                    <img className={styles.imgCrianca} src={fotoCrianca} alt="Uma criança usando máscara sobre os olhos" />
                 </div>
             </div>
           </div>
