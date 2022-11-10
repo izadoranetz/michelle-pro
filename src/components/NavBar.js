5e7489e6a10fea41246478d42a64112e21a91952
izadoranetz <izadora.ns@gmail.com>
2022-11-09 23:13:18 -0300

    ajustes componente navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import './Navbar.scss';
 import '../assets/fonts/fonts.css';
+import * as styles from './Navbar.module.scss';
 
 const isActive = ({ isCurrent }) => {
   return isCurrent
@@ -16,10 +17,10 @@ const ExactNavLink = (props) => <Link getProps={isActive} {...props} />;
 const Navbar = () => {
   return (
     
-    <nav className="navbar navbar-expand-md navbar-dark">
+    <nav className={`navbar navbar-expand-md navbar-dark ${styles.bgNav}`}>
       <div className="container-fluid">
 
-        <Link to="/" className="navbar-brand" href="#">
+        <Link to="/" className={`navbar-brand ${styles.navbarCinza}`} href="#">
           <StaticImage
             src="../assets/images/logo.svg"
             alt="Michelle Procópio"
@@ -74,3 +75,82 @@ Navbar.defaultProps = {
 };
 
 export default Navbar;
+
+
+
+// import * as React from 'react';
+// import PropTypes from 'prop-types';
+// import { Link } from 'gatsby';
+// import { StaticImage } from 'gatsby-plugin-image';
+// import './Navbar.scss';
+// import '../assets/fonts/fonts.css';
+
+// const isActive = ({ isCurrent }) => {
+//   return isCurrent
+//     ? { className: 'nav-link active' }
+//     : { className: 'nav-link' };
+// };
+
+// const ExactNavLink = (props) => <Link getProps={isActive} {...props} />;
+
+// const Navbar = () => {
+//   return (
+    
+//     <nav className="navbar navbar-expand-md navbar-dark">
+//       <div className="container-fluid">
+
+//         <Link to="/" className="navbar-brand" href="#">
+//           <StaticImage
+//             src="../assets/images/logo.svg"
+//             alt="Michelle Procópio"
+//             layout="fixed"
+//             height={80}
+//             id="logoMichelle"
+//           />
+//         </Link>
+
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#main-navbar"
+//           aria-controls="main-navbar"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+
+//         <div className="collapse navbar-collapse" id="main-navbar">
+//           <ul className="navbar-nav ml-auto">
+//             <li className="nav-item">
+//               <ExactNavLink to="/">Home</ExactNavLink>
+//             </li>
+//             <li className="nav-item">
+//               <ExactNavLink to="/servicos">Serviços</ExactNavLink>
+//             </li>
+//             <li className="nav-item">
+//               <ExactNavLink to="/metodo-denver">Método Denver</ExactNavLink>
+//             </li>
+//             <li className="nav-item">
+//               <ExactNavLink to="/sobre-nos">Sobre Nós</ExactNavLink>
+//             </li>
+//             <li className="nav-item">
+//               <ExactNavLink to="/equipe">Equipe</ExactNavLink>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// Navbar.propTypes = {
+//   siteTitle: PropTypes.string,
+// };
+
+// Navbar.defaultProps = {
+//   siteTitle: ``,
+// };
+
+// export default Navbar;
