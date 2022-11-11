b69bf97109cd903ef4a4997ded5b90857a592caa
izadoranetz <izadora.ns@gmail.com>
2022-11-11 20:11:43 -0300

    feature grandes números e ajustes

---
+++ src/components/GrandesNumeros.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as styles from './GrandesNumeros.module.scss';
+
+function GrandesNumeros() {
+  return (
+    <section className={`${styles.containerNumeros}`}>
+      <div className={styles.fotoCriancas}>
+        <div className={styles.detalheTop}></div>
+        <div className={styles.detalheVermelhoTop}></div>
+      </div>
+      <div className="container">
+        <div className="row py-5">
+          <div className="col-lg-3">
+            <h3 className={styles.grandesNumeros}>+80</h3>
+            <span className={styles.numerosLegenda}>Pacientes atendidos</span>
+          </div>
+          <div className="col-lg-3">
+            <h3 className={styles.grandesNumeros}>+60</h3>
+            <span className={styles.numerosLegenda}>Profissionais capacitados</span>
+          </div>
+          <div className="col-lg-3">
+            <h3 className={styles.grandesNumeros}>+20</h3>
+            <span className={styles.numerosLegenda}>Certificados internacionais</span>
+          </div>
+          <div className="col-lg-3">
+            <h3 className={styles.grandesNumeros}>+500</h3>
+            <span className={styles.numerosLegenda}>Coach parental realizados</span>
+          </div>
+        </div>
+        <div className="row py-5">
+          <div className="col-lg-12">
+            <a href="#" className={styles.btnAgendar}>Agendar uma consulta</a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.detalheVermelhoBottom}></div>
+    </section>
+  );
+}
+
+export default GrandesNumeros;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 import HeroSection from '../components/HeroSection';
 import Carrossel from '../components/Carrossel';
 import Box from '../components/Box';
+import GrandesNumeros from '../components/GrandesNumeros';
 
 const IndexPage = () => (
   <Layout>
     <HeroSection />
     <Carrossel />
     <Box />
+    <GrandesNumeros />
   </Layout>
 );
 
