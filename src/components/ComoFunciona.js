a7e7016c8c2be6b41da2330b1978112f8b1aafe9
izadoranetz <izadora.ns@gmail.com>
2022-12-21 11:15:28 -0300

    alinhamento

--- src/components/ComoFunciona.js
+++ src/components/ComoFunciona.js
@@ -11,83 +11,94 @@ function ComoFunciona() {
     <section className={styles.sectionComoFunciona}>
       <div className="container">
         <TituloNuvem titulo="Mas como funciona?" cor="fundoAzul" />
-        <div className="row justify-content-evenly flex-wrap">
-          <div
-            className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
-          >
-            <div className={`${styles.cardNumeracao} ${styles.fundoVermelho}`}>
-              1
-            </div>
+        <div className="container">
+          <div className="row justify-content-evenly flex-wrap">
             <div
-              className={`${styles.cardComoFuncionaTitulo} ${styles.vermelho}`}
+              className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
             >
-              <span>Avaliação</span>
+              <div
+                className={`${styles.cardNumeracao} ${styles.fundoVermelho}`}
+              >
+                1
+              </div>
+              <div
+                className={`${styles.cardComoFuncionaTitulo} ${styles.vermelho}`}
+              >
+                <span>Avaliação</span>
+              </div>
+              <div className={styles.cardComoFuncionaImagem}>
+                <img
+                  src={imgItem1}
+                  alt="Foto criança brincando com carrinhos"
+                />
+              </div>
+              <div className={styles.cardComoFuncionaTextos}>
+                Utilizamos o Checklist do Método Denver (ESDM)
+              </div>
             </div>
-            <div className={styles.cardComoFuncionaImagem}>
-              <img src={imgItem1} alt="Foto criança brincando com carrinhos" />
-            </div>
-            <div className={styles.cardComoFuncionaTextos}>
-              Utilizamos o Checklist do Método Denver (ESDM)
-            </div>
-          </div>
 
-          <div
-            className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
-          >
-            <div className={`${styles.cardNumeracao} ${styles.fundoAmarelo}`}>
-              2
-            </div>
             <div
-              className={`${styles.cardComoFuncionaTitulo} ${styles.amarelo}`}
+              className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
             >
-              <span>Plano de Terapia</span>
+              <div className={`${styles.cardNumeracao} ${styles.fundoAmarelo}`}>
+                2
+              </div>
+              <div
+                className={`${styles.cardComoFuncionaTitulo} ${styles.amarelo}`}
+              >
+                <span>Plano de Terapia</span>
+              </div>
+              <div className={styles.cardComoFuncionaImagem}>
+                <img src={imgItem2} alt=" " />
+              </div>
+              <div className={styles.cardComoFuncionaTextos}>
+                Desenvolvimento individualizado contendo objetivos de todos os
+                domínios do desenvolvimento.
+              </div>
             </div>
-            <div className={styles.cardComoFuncionaImagem}>
-              <img src={imgItem2} alt=" " />
-            </div>
-            <div className={styles.cardComoFuncionaTextos}>
-              Desenvolvimento individualizado contendo objetivos de todos os
-              domínios do desenvolvimento.
-            </div>
-          </div>
 
-          <div
-            className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
-          >
-            <div className={`${styles.cardNumeracao} ${styles.fundoAzul}`}>
-              3
-            </div>
-            <div className={`${styles.cardComoFuncionaTitulo} ${styles.azul}`}>
-              <span>Intervenção Precoce</span>
-            </div>
-            <div className={styles.cardComoFuncionaImagem}>
-              <img src={imgItem3} alt=" " />
-            </div>
-            <div className={styles.cardComoFuncionaTextos}>
-              Objetivos desenvolvidos para serem adquiridos num período médio de
-              12 semanas. A meta é definida e adaptada durante as semanas pela
-              supervisora do caso.
-            </div>
-          </div>
-
-          <div
-            className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
-          >
-            <div className={`${styles.cardNumeracao} ${styles.fundoVermelho}`}>
-              4
-            </div>
             <div
-              className={`${styles.cardComoFuncionaTitulo} ${styles.vermelho}`}
+              className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
             >
-              <span>Resultados</span>
+              <div className={`${styles.cardNumeracao} ${styles.fundoAzul}`}>
+                3
+              </div>
+              <div
+                className={`${styles.cardComoFuncionaTitulo} ${styles.azul}`}
+              >
+                <span>Intervenção Precoce</span>
+              </div>
+              <div className={styles.cardComoFuncionaImagem}>
+                <img src={imgItem3} alt=" " />
+              </div>
+              <div className={styles.cardComoFuncionaTextos}>
+                Objetivos desenvolvidos para serem adquiridos num período médio
+                de 12 semanas. A meta é definida e adaptada durante as semanas
+                pela supervisora do caso.
+              </div>
             </div>
-            <div className={styles.cardComoFuncionaImagem}>
-              <img src={imgItem4} alt=" " />
-            </div>
-            <div className={styles.cardComoFuncionaTextos}>
-              A cada 12 semandas será realizada uma nova avaliação para o
-              próximo plano terapêutico. Novos objetivos serão estabelecidos ou
-              os antigos serão revistos.
+
+            <div
+              className={`col-lg-4 col-md-12 cold-sm-12 ${styles.cardComoFunciona}`}
+            >
+              <div
+                className={`${styles.cardNumeracao} ${styles.fundoVermelho}`}
+              >
+                4
+              </div>
+              <div
+                className={`${styles.cardComoFuncionaTitulo} ${styles.vermelho}`}
+              >
+                <span>Resultados</span>
+              </div>
+              <div className={styles.cardComoFuncionaImagem}>
+                <img src={imgItem4} alt=" " />
+              </div>
+              <div className={styles.cardComoFuncionaTextos}>
+                A cada 12 semandas será realizada uma nova avaliação para o
+                próximo plano terapêutico. Novos objetivos serão estabelecidos
+                ou os antigos serão revistos.
+              </div>
             </div>
           </div>
         </div>
