63a8ce801fe8a230e355b1a48dbb5b30b9417650
izadoranetz <izadora.ns@gmail.com>
2022-12-22 11:41:02 -0300

    feat(Método Denver): :construction: componente sobre método denver. ajustes no warning chunk commons
    ajuste warn chunk commons conforme tutorial https://robertmarshall.dev/blog/fix-warn-chunk-commons-mini-css-extract-plugin-error-in-gatsby-js/

--- src/components/BoxOqueEModeloDenver.js
+++ src/components/BoxOqueEModeloDenver.js
@@ -1,19 +1,46 @@
 import * as React from 'react';
 import * as styles from './BoxOqueEModeloDenver.module.scss';
 import TituloNuvem from './TituloNuvem';
+import tresEstrelas from '../assets/images/tres-estrelas.png';
 
 function BoxOqueEModeloDenver() {
   return (
     <section className={`container ${styles.sectionModeloDenver}`}>
-      <div className={`row ${styles.containerModeloDenver}`}>
-        <TituloNuvem titulo="O que é o Modelo Denver?" cor="fundoAzul" />
-        <div className={styles.boxDescricao}>
-          <p>
-            O Modelo Denver é um programa que combina as melhores práticas da
-            Análise do Comportamento Aplicada (ABA) com uma abordagem baseada na
-            rotina, no jogo e no relacionamento, que torna a terapia divertida e
-            eficaz.
-          </p>
+      <div className="container">
+        <div className={`row ${styles.containerModeloDenver}`}>
+          <div className={`${styles.containerModeloDenver}`}>
+            <TituloNuvem titulo="O que é o Modelo Denver?" cor="fundoAzul" />
+            <div className={styles.boxDescricao}>
+              <p>
+                O Modelo Denver é um programa que combina as melhores práticas
+                da Análise do Comportamento Aplicada (ABA) com uma abordagem
+                baseada na rotina, no jogo e no relacionamento, que torna a
+                terapia divertida e eficaz.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={`container ${styles.rowDetalhes}`}>
+        <img className={styles.tresEstrelas} src={tresEstrelas} alt=" " />
+        <div className="row justify-content-evenly flex-wrap">
+          <div className={`col-lg-5 ${styles.boxDetalhes} ${styles.boxAzul}`}>
+            <p className={styles.boxTexto}>
+              O modelo se destaca por ajudar as crianças com autismo se tornem
+              participantes mais ativos no mundo. Iniciando e aceitando
+              interações com outras pessoas, pois as interações sociais estão
+              diretamente relacionadas as oportunidades de aprendizagem.
+            </p>
+          </div>
+          <div
+            className={`col-lg-5 ${styles.boxDetalhes} ${styles.boxVermelho}`}
+          >
+            <p className={styles.boxTexto}>
+              Nosso foco é trazer a criança para uma relação social, interativa
+              e coordenada durante a maior parte do tempo do seu dia, pois a
+              aprendizagem se dá nas trocas sociais.
+            </p>
+          </div>
         </div>
       </div>
     </section>
