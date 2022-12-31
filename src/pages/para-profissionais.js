5e62ac8bdad4b2db64576cc287ead4a0ff277b82
izadoranetz <izadora.ns@gmail.com>
2022-12-31 20:15:15 -0300

    página para profissionais

--- src/pages/para-profissionais.js
+++ src/pages/para-profissionais.js
@@ -1,10 +1,43 @@
 import * as React from 'react';
+import Card from '../components/Card';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import * as styles from './paraProfissionais.module.scss';
 
 const ParaProfissionais = () => (
   <Layout>
-    <h1>Para profissionais</h1>
+    <section className={styles.mainProfissionais}>
+      <div className="container">
+        <div className={styles.tituloPrincipal}>
+          <span>Para profissionais</span>
+        </div>
+      </div>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-lg-4 col-sm-12 col-md-12 mb-4">
+            <Card
+              titulo="Supervisão"
+              texto="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut  diam nonummy nibh tincidunt ut  "
+              cor="azul"
+            />
+          </div>
+          <div className="col-lg-4 col-sm-12 col-md-12 mb-4">
+            <Card
+              titulo="Treinamento e Consultoria"
+              texto="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut  diam nonummy nibh tincidunt ut  "
+              cor="vermelho"
+            />
+          </div>
+          <div className="col-lg-4 col-sm-12 col-md-12 mb-4">
+            <Card
+              titulo="Cursos"
+              texto="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut  diam nonummy nibh tincidunt ut  "
+              cor="amarelo"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
