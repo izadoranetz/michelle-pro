6033bc9d00209e99483e10b72e8e065f8c900ed6
Yuri da Mata <yuridamata@gmail.com>
2023-02-04 18:03:43 -0300

    COrreções

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,12 @@ const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
+    resolve:{
+      fallback: {
+        timers: require.resolve("timers-browserify"),
+        zlib:  require.resolve("browserify-zlib")
+      }
+    },
     plugins: [
       new FilterWarningsPlugin({
         exclude:
